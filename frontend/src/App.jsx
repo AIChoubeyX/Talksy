f7a1940ff60ead91b6a6e9554cdd87e92edd0783
Chooubeyji <ashutoshchoubey2004@gmail.com>
2025-04-24 11:49:14 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -46,7 +46,7 @@
 
 // export default App;
 import React, { useEffect } from "react";
-import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
+import { Routes, Route, Navigate} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
@@ -64,28 +64,21 @@ const App = () => {
     }, []);
 
     return (
-        <BrowserRouter>
-            <Navbar />
+      <div>
+        
+      <Navbar />
 
-            <Routes>
-                {/* Home Page - protected route */}
-                <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+      <Routes>
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
+      </Routes>
 
-                {/* Sign Up Page - only for unauthenticated users */}
-                <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-
-                {/* Login Page - only for unauthenticated users */}
-                <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
-
-                {/* Settings Page - always accessible */}
-                <Route path="/settings" element={<SettingsPage />} />
-
-                {/* Profile Page - protected route */}
-                <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
-            </Routes>
-
-            <Toaster />
-        </BrowserRouter>
+      <Toaster />
+      </div>
+    
     );
 };
 
