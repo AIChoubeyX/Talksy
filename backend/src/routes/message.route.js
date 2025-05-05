be7ae1621034314016019ecb5007ea3f8187a2d3
Chooubeyji <ashutoshchoubey2004@gmail.com>
2025-05-05 23:27:05 +0530

    new

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -12,19 +12,38 @@
 // export default router;
 
 
+// import express from "express";
+// import { protectRoute } from "../middlewares/auth.middleware.js";
+// import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+
+// const router = express.Router();
+
+// // Get all users except logged-in user
+// router.get("/users", protectRoute, getUsersForSidebar);
+
+// // Get chat messages between two users
+// router.get("/:id", protectRoute, getMessages);
+
+// // Send a message to a user
+// router.post("/send/:id", protectRoute, sendMessage);
+
+// export default router;
 import express from "express";
 import { protectRoute } from "../middlewares/auth.middleware.js";
-import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-// Get all users except logged-in user
+// Specific routes first
 router.get("/users", protectRoute, getUsersForSidebar);
 
-// Get chat messages between two users
-router.get("/:id", protectRoute, getMessages);
-
-// Send a message to a user
+// Use scoped dynamic route
+router.get("/chat/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
+
